19b3ffec9ae649402392ca061678975cbf1ba208
Mayankchauhan129 <mayankchauhan129@gmail.com>
2021-02-28 22:34:12 +0530

    First Commit

---
+++ src/main/java/LoginPage.java
@@ -0,0 +1,60 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class LoginPage {
+	
+	public static WebDriver driver;
+	public static Properties prop;
+	static String url ;
+
+	public static void start(String url) throws IOException, ParseException {
+
+		
+		prop = new Properties();
+		FileInputStream ip = new FileInputStream(".\\src\\main\\java\\config\\config.properties");
+		prop.load(ip);
+
+		String browserName = (String) prop.get("browser");
+
+		if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", ".\\src\\test\\resources\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browserName.equals("FF")) {
+			System.setProperty("webdriver.gecko.driver", ".\\src\\test\\resources\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+
+		driver.manage().window().maximize();
+
+		String a = url;
+		
+		driver.get(a);
+
+	}
+	
+	public String validateLoginPage() throws InterruptedException {
+		WebElement email = driver.findElement(By.xpath("//*[@type='email']"));
+		email.sendKeys("qaenggtools@gmail.com");
+		Thread.sleep(3000);
+		WebElement SignInButton =driver.findElement(By.xpath("//div[@class='VfPpkd-RLmnJb']"));
+		SignInButton.click();
+		WebElement password =driver.findElement(By.xpath("//*[@type='password']"));
+		password.sendKeys("Mayank@129");
+		WebElement SignInButton1 =driver.findElement(By.xpath("//span[contains(text(),'Next')]"));
+		SignInButton1.click();
+		WebElement element = driver.findElement(By.xpath("//*[@aria-label='Account Information']//div[@class='gb_ob']"));
+		String accountEmailId = element.getText();
+		return accountEmailId;
+
+	}
+
+}
---
+++ src/test/java/LoginPageTest.java
@@ -0,0 +1,46 @@
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginPageTest extends LoginPage {
+
+	public static WebDriver driver;
+	String emailId;
+	String pwd;
+	String url;
+	
+	@BeforeMethod
+	public void setup() throws IOException, ParseException {
+	JSONParser jsonparser = new JSONParser();
+
+	FileReader reader = new FileReader(".\\jsonfiles\\Data.json");
+
+	JSONObject obj = (JSONObject) jsonparser.parse(reader);
+
+	emailId = (String) obj.get("Email Id");
+	pwd = (String) obj.get("Password");
+	url = (String) obj.get("URL");
+	start(url);
+	}
+	
+	@Test(priority=1)
+	public void loginPageTitleTest() throws InterruptedException {
+		
+		validateLoginPage();
+//		Assert.assertEquals(details, "qaenggtools@gmail.com");
+	}
+	
+	@AfterMethod
+	public void afterTest() {
+		driver.quit();			
+	}
+	
+}
